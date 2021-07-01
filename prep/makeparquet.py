195a6d8e61fdc81668d8539335550918c4296773
Jim Pivarski <jpivarski@gmail.com>
2021-06-30 20:00:59 -0500

    Figuring out what I'm going to do in the Awkward part.

--- prep/makeparquet.py
+++ prep/makeparquet.py
@@ -4,17 +4,64 @@ import uproot
 oldfile = uproot.open("/home/jpivarski/Downloads/2011MCNtuples.root")
 oldtree = oldfile["aod2nanoaod/Events"]
 
-HLT_IsoMu24 = oldtree["HLT_IsoMu24"].array()
-HLT_IsoMu24_eta2p1 = oldtree["HLT_IsoMu24_eta2p1"].array()
-HLT_IsoMu17_eta2p1_LooseIsoPFTau20 = oldtree["HLT_IsoMu17_eta2p1_LooseIsoPFTau20"].array()
+run = oldtree["run"].array()
+luminosityBlock = oldtree["luminosityBlock"].array()
+event = oldtree["event"].array()
+MET_pt = oldtree["MET_pt"].array()
+MET_phi = oldtree["MET_phi"].array()
 Muon_pt = oldtree["Muon_pt"].array()
 Muon_eta = oldtree["Muon_eta"].array()
 Muon_phi = oldtree["Muon_phi"].array()
 Muon_mass = oldtree["Muon_mass"].array()
 Muon_charge = oldtree["Muon_charge"].array()
+GenPart_pt = oldtree["GenPart_pt"].array()
+GenPart_eta = oldtree["GenPart_eta"].array()
+GenPart_phi = oldtree["GenPart_phi"].array()
+GenPart_pdgId = oldtree["GenPart_pdgId"].array()
 
-hlt = ak.zip({"IsoMu24": HLT_IsoMu24, "IsoMu24_eta2p1": HLT_IsoMu24_eta2p1, "IsoMu17_eta2p1_LooseIsoPFTau20": HLT_IsoMu17_eta2p1_LooseIsoPFTau20})
-muons = ak.zip({"pt": Muon_pt, "eta": Muon_eta, "phi": Muon_phi, "mass": Muon_mass, "charge": Muon_charge})
-array = ak.Array({"hlt": hlt, "muons": muons})
+met = ak.Array({"pt": MET_pt, "phi": MET_phi})
+muons = ak.zip(
+    {
+        "pt": Muon_pt,
+        "eta": Muon_eta,
+        "phi": Muon_phi,
+        "mass": Muon_mass,
+        "charge": Muon_charge,
+    }
+)
+gen = ak.zip(
+    {
+        "pt": GenPart_pt,
+        "eta": GenPart_eta,
+        "phi": GenPart_phi,
+        "pdgId": GenPart_pdgId,
+    }
+)
+array = ak.Array({
+    "run": run,
+    "luminosityBlock": luminosityBlock,
+    "event": event,
+    "MET": met,
+    "muons": muons,
+    "gen": gen,
+})
 
-ak.to_parquet(array, "HiggsZZ4mu.parquet", list_to32=True, use_dictionary=False, compression="ZSTD", compression_level=9, use_byte_stream_split=["muons.pt", "muons.eta", "muons.phi", "muons.mass"])
+ak.to_parquet(
+    array,
+    "HiggsZZ4mu.parquet",
+    list_to32=True,
+    use_dictionary=False,
+    compression="ZSTD",
+    compression_level=9,
+    use_byte_stream_split=[
+        "MET.pt",
+        "MET.phi",
+        "muons.pt",
+        "muons.eta",
+        "muons.phi",
+        "muons.mass",
+        "gen.pt",
+        "gen.eta",
+        "gen.phi",
+    ],
+)
--- prep/makeroot.C
+++ prep/makeroot.C
@@ -4,14 +4,20 @@ void makeroot() {
   oldfile->GetObject("aod2nanoaod/Events", oldtree);
 
   oldtree->SetBranchStatus("*", 0);
-  oldtree->SetBranchStatus("HLT_IsoMu24", 1);
-  oldtree->SetBranchStatus("HLT_IsoMu24_eta2p1", 1);
-  oldtree->SetBranchStatus("HLT_IsoMu17_eta2p1_LooseIsoPFTau20", 1);
+  oldtree->SetBranchStatus("run", 1);
+  oldtree->SetBranchStatus("luminosityBlock", 1);
+  oldtree->SetBranchStatus("event", 1);
+  oldtree->SetBranchStatus("MET_pt", 1);
+  oldtree->SetBranchStatus("MET_phi", 1);
   oldtree->SetBranchStatus("Muon_pt", 1);
   oldtree->SetBranchStatus("Muon_eta", 1);
   oldtree->SetBranchStatus("Muon_phi", 1);
   oldtree->SetBranchStatus("Muon_mass", 1);
   oldtree->SetBranchStatus("Muon_charge", 1);
+  oldtree->SetBranchStatus("GenPart_pt", 1);
+  oldtree->SetBranchStatus("GenPart_eta", 1);
+  oldtree->SetBranchStatus("GenPart_phi", 1);
+  oldtree->SetBranchStatus("GenPart_pdgId", 1);
 
   auto newfile = TFile::Open("HiggsZZ4mu.root", "recreate");
   newfile->SetCompressionAlgorithm(ROOT::kZSTD);
